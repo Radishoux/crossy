90a084a57b7027ca55d7df8df6cda97231eb74c5
Rudy Quinternet <rudy.quinternet@gmail.com>
2023-12-19 19:39:03 +0100

    un bon debut

---
+++ cw.js
@@ -0,0 +1,13 @@
+module.exports = {
+  generate: function (words = [], clues = []) {
+
+    var matrix = [];
+
+    // getFittingWords
+    // read mattrix, if empty return rand word else return a list of words with fitting letters
+    // select word rand
+    // place word in matrix and pop from list
+    // repeat until list is empty or no fitting words are found
+    return "This is a crossword!"
+  },
+};
---
+++ index.js
@@ -0,0 +1,8 @@
+var cw = require('./cw.js');
+
+
+
+
+console.log(cw.generate());
+console.log(cw.generate(1, 2));
+
