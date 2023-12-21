c168a32d1ed8514da6ac7f6283e12a6163cbe20f
Rudy Quinternet <rudy.quinternet@gmail.com>
2023-12-21 18:28:43 +0100

    Delete VSCode settings and add new functions to cw.js

--- cw.js
+++ cw.js
@@ -1,7 +1,50 @@
 module.exports = {
+  checkWordMatrix: function (matrix = [[]], words = []) {
+
+  },
+
+  getFittingWords: function (matrix = [[]], words = []) {
+
+    if (matrix.length == 0) {
+      return words;
+    }
+
+    // get letters used in matrix
+
+    var letters = [];
+    for (var i = 0; i < matrix.length; i++) {
+      for (var j = 0; j < matrix[i].length; j++) {
+        letters.push(matrix[i][j]);
+      }
+    }
+
+    // clear letters from duplicates
+
+    letters = letters.filter((item, pos) => { return letters.indexOf(item) == pos; });
+
+    // filter words with fitting letters
+    var similarLetterWords = [];
+    for (var i = 0; i < words.length; i++) {
+      for (var j = 0; j < letters.length; j++) {
+        if (words[i].includes(letters[j])) {
+          similarLetterWords.push(words[i]);
+          break;
+        }
+      }
+    }
+
+    var fittingWords = {
+      // potato: { vertical: [[-1, 14]], horizontal: [[8, -3]] },
+    };
+
+
+
+    return fittingWords;
+  },
+
   generate: function (words = [], clues = []) {
 
-    var matrix = [];
+    var matrix = [[]];
 
     // getFittingWords
     // read mattrix, if empty return rand word else return a list of words with fitting letters
